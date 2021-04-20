2e38be0c517a0c895082aacbb1eed407565df861
akinmayowa <mayowa@gmail.com>
2021-04-20 07:01:40 +0100

    initial refactor

---
+++ web/http.go
@@ -0,0 +1,20 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Response(w http.ResponseWriter, message string) {
+	fmt.Fprintf(w, message)
+}
+
+func Error(w http.ResponseWriter, message string, status ...int) {
+	statusCode := 500
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	w.WriteHeader(statusCode)
+	fmt.Fprintf(w, message)
+}
