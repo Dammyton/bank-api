16595426f8bb16869bb51738808073601104a690
Dammyton <owolabidamilola98@gmail.com>
2021-06-22 12:58:21 +0100

    test file

--- services/bank/bank_test.go
+++ services/bank/bank_test.go
@@ -1,98 +1,91 @@
 package bank
 
-import (
-	"fmt"
-	"testing"
+// func TestDeposit(t *testing.T) {
+// 	account := Account{
+// 		Customer: Customer{
+// 			Name:    "John",
+// 			Address: "Los Angeles, California",
+// 			Phone:   "(213) 555 0147",
+// 		},
+// 		Number:  100000000001,
+// 		Balance: decimal.NewFromInt(0),
+// 	}
 
-	"github.com/shopspring/decimal"
-)
+// 	account.Deposit(decimal.NewFromInt(500))
 
-func TestTransfer(t *testing.T) {
-	accountA := Account{
-		Customer: Customer{
-			Name:    "John",
-			Address: "Los Angeles, California",
-			Phone:   "(213) 555 0147",
-		},
-		Number:  100000000001,
-		Balance: decimal.NewFromInt(0),
-	}
+// 	if account.Balance.Cmp(decimal.NewFromInt(500)) != 0 {
+// 		t.Error("balance is not being updated after a deposit")
+// 	}
+// }
 
-	accountB := Account{
-		Customer: Customer{
-			Name:    "Mark",
-			Address: "Irvine, California",
-			Phone:   "(949) 555 0198",
-		},
-		Number:  100000000002,
-		Balance: decimal.NewFromInt(0),
-	}
+// func TestWithdraw(t *testing.T) {
+// 	account := Account{
+// 		Customer: Customer{
+// 			Name:    "John",
+// 			Address: "Los Angeles, California",
+// 			Phone:   "(213) 555 0147",
+// 		},
+// 		Number:  100000000001,
+// 		Balance: decimal.NewFromInt(0),
+// 	}
+// 	account.Deposit(decimal.NewFromInt(1000))
+// 	account.Withdraw(decimal.NewFromInt(100))
 
-	accountA.Deposit(decimal.NewFromInt(100))
-	err := accountA.Transfer(decimal.NewFromInt(50), &accountB)
+// 	if account.Balance.Cmp(decimal.NewFromInt(900)) != 0 {
+// 		t.Error("balance is not being updated after withdraw")
+// 	}
 
-	if accountA.Balance.Cmp(decimal.NewFromInt(50)) != 0 {
-		t.Error("transfer from account A to account B is not working", err)
-	}
-}
+// }
 
-func TestDeposit(t *testing.T) {
-	account := Account{
-		Customer: Customer{
-			Name:    "John",
-			Address: "Los Angeles, California",
-			Phone:   "(213) 555 0147",
-		},
-		Number:  100000000001,
-		Balance: decimal.NewFromInt(0),
-	}
+// func TestTransfer(t *testing.T) {
+// 	accountA := Account{
+// 		Customer: Customer{
+// 			Name:    "John",
+// 			Address: "Los Angeles, California",
+// 			Phone:   "(213) 555 0147",
+// 		},
+// 		Number:  100000000001,
+// 		Balance: decimal.NewFromInt(0),
+// 	}
 
-	account.Deposit(decimal.NewFromInt(500))
+// 	accountB := Account{
+// 		Customer: Customer{
+// 			Name:    "Mark",
+// 			Address: "Irvine, California",
+// 			Phone:   "(949) 555 0198",
+// 		},
+// 		Number:  100000000002,
+// 		Balance: decimal.NewFromInt(0),
+// 	}
 
-	if account.Balance.Cmp(decimal.NewFromInt(500)) != 0 {
-		t.Error("balance is not being updated after a deposit")
-	}
-}
+// 	accountA.Deposit(decimal.NewFromInt(100))
+// 	err := accountA.Transfer(decimal.NewFromInt(50), &accountB)
 
-func TestWithdraw(t *testing.T) {
-	account := Account{
-		Customer: Customer{
-			Name:    "John",
-			Address: "Los Angeles, California",
-			Phone:   "(213) 555 0147",
-		},
-		Number:  100000000001,
-		Balance: decimal.NewFromInt(0),
-	}
-	account.Deposit(decimal.NewFromInt(1000))
-	account.Withdraw(decimal.NewFromInt(100))
+// 	if accountA.Balance.Cmp(decimal.NewFromInt(50)) != 0 {
+// 		t.Error("transfer from account A to account B is not working", err)
+// 	}
+// }
 
-	if account.Balance.Cmp(decimal.NewFromInt(900)) != 0 {
-		t.Error("balance is not being updated after withdraw")
-	}
+// func TestStatement(t *testing.T) {
+// 	account := Account{
+// 		Customer: Customer{
+// 			Name:    "John",
+// 			Address: "Los Angeles, California",
+// 			Phone:   "(213) 555 0147",
+// 		},
+// 		Number:  100000000001,
+// 		Balance: decimal.NewFromInt(0),
+// 	}
 
-}
+// 	account.Deposit(decimal.NewFromInt(100))
+// 	statement := account.Statement()
 
-func TestStatement(t *testing.T) {
-	account := Account{
-		Customer: Customer{
-			Name:    "John",
-			Address: "Los Angeles, California",
-			Phone:   "(213) 555 0147",
-		},
-		Number:  100000000001,
-		Balance: decimal.NewFromInt(0),
-	}
+// 	if statement != "100000000001 - John - 100" {
+// 		t.Error("statement doesn't have the proper format")
+// 	}
+// }
 
-	account.Deposit(decimal.NewFromInt(100))
-	statement := account.Statement()
-
-	if statement != "100000000001 - John - 100" {
-		t.Error("statement doesn't have the proper format")
-	}
-}
-
-// Statement ...
-func (a *Account) Statement() string {
-	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
-}
+// // Statement ...
+// func (a *Account) Statement() string {
+// 	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
+// }
